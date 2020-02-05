1ae99f2a9b65d105dc03a2bbd6627a1ce6de44a1
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-05 21:17:30 +0000

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+#include <unisd.h>
+/**
+ * main - run all function
+ * 
+ * Return: Return 1 and exit
+ */
+int main(void)
+{
+int l;
+l =strln("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2 ,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",l);
+return (1);
+}
