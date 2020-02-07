13529a1c728a00a7485fc2beb575c8c2f6cc4261
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-07 12:51:56 +0100

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int Al;
+
+	for (Al = 0; Al <= 9; Al++)
+	{
+		putchar(Al +'0');
+		if (Al != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
