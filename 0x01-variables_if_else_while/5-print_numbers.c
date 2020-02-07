e872a0a9b59d56d7e81048994bc0b918b41fac7e
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-07 12:16:11 +0100

    commit*

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - single digit numbers base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char Al;
+
+	for (Al = '0'; Al <= '9'; Al++)
+		putchar(Al);
+	putchar('\n');
+
+	return (0);
+}
