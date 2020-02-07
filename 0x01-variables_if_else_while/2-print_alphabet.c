20f3a2544e83c1844e52d95116a9eb94c54a6d06
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-07 11:30:51 +0100

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char Al;
+
+	for (Al = 'a'; Al <= 'z'; Al++)
+		putchar(Al);
+	putchar('\n');
+
+	return (0);
+}
