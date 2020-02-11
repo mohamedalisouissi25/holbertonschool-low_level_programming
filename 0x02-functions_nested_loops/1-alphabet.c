d49db32be063962e02fa47a001303c4b3d768f40
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-11 11:46:30 +0100

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - lowercase alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char Al;
+for (Al = 97; Al <= 122; Al++)
+_putchar(Al);
+_putchar('\n');
+}
