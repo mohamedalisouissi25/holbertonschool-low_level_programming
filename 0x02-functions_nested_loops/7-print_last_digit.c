8321f9a912e38bf965b098587c79fc8e9507600c
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-12 08:34:11 +0100

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value
+ * @n: input
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+int l = n % 10;
+if (l >= 0)
+{
+_putchar(l + '0');
+return (l);
+}
+else
+{
+_putchar(l * -1 + '0');
+return (l * -1);
+}
+}
