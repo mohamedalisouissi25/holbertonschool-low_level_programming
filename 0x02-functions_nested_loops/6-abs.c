58d751e0629c89f9b8ad82045063e195240f0ee5
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-12 08:14:00 +0100

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - computes the absolute value
+ * @n: input
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n *= -1);
+}
+}
