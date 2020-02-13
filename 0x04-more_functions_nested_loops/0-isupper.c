a5a800a6b5a200267314677b2c17dad3c93bbbc1
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-13 10:49:10 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - cheks uppercase
+ * @Al: input
+ * Return: 1 if uppercase otherwise 0
+ */
+int _isupper(int Al)
+{
+if (Al >= 'A' && Al <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
