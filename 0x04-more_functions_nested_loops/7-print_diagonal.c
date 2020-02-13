5d3d5250232ba1f4084728163b54ef0ed773d872
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-13 15:52:05 +0100

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,24 @@
 #include "holberton.h"
 
 /**
- * print_diagonal - check the code for Holberton School students.
+ * print_diagonal - draws a diagonal
  * @n : input
  * Return: output
  */
 void print_diagonal(int n)
 {
 int l, m;
+if (n > 0)
+{
 for (l = 0; l < n; l++)
 {
 for (m = 0; m < l; m++)
 {
 _putchar(' ');
 }
-}
 _putchar('\\');
+_putchar('\n');
+}
+}
 _putchar('\n');
 }
