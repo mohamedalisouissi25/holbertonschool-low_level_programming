5710bd10d804b0c0e2c69fa8621a848a3284e685
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-13 11:07:46 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - cheks uppercase
+ * @Al: input
+ * Return: 1 if digit otherwise 0
+ */
+int _isdigit(int Al)
+{
+if (Al == '0' && Al <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
