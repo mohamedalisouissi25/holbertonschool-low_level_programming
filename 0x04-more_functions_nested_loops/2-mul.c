445875593ea0c1c7cbc90d62a5ef9318fbb58c58
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-13 11:13:56 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two ints
+ * @a: input
+ * @b: input
+ * Return: multiplie ints
+ */
+int mul(int a, int b)
+{
+int Al;
+Al = (a * b);
+return (Al);
+}
