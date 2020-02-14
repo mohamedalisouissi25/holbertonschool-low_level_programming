de90bc5729b72149994149911b9ed123098d5e94
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-14 09:40:17 +0100

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,19 +7,20 @@
 void print_triangle(int size)
 {
 int n, m;
-if (size > 0)
+	if (size > 0)
 {
-for (n = 1; n <= size; n++)
+		for (n = 1; n <= size; n++)
 {
-for (m = size; m >= 1; m--)
+		for (m = size; m >= 1; m--)
 {
-if (n < m)
-_putchar(' ');
-else
-_putchar('#');
+			if (n < m)
+			_putchar(' ');
+			else
+			_putchar('#');
 }
-_putchar('\n');
+		_putchar('\n');
 }
-else
-_putchar('\n');
+}
+	else
+	_putchar('\n');
 }
