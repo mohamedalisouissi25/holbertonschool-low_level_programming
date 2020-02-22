19785175f19051a3e7c63d85e71bc885d0194fc0
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-22 17:03:51 +0100

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* *_strncat - concatenates
+* @dest: input destş
+* @src: input
+* @n: input
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+int i, j;
+	for (i = 0; dest[i] != '\0'; i++)
+{
+}
+
+j = 0;
+	while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+dest[i + 1] = '\0';
+return (dest);
+}
