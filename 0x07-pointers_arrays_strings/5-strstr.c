c7099595ee02eec2da6add3ddf77d88460ede9a4
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-02-24 21:16:49 +0100

    commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: output
+ */
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int i = 0, k = 0;
+
+while (haystack[i])
+{
+while (needle[k] && (haystack[i] == needle[0]))
+{
+if (haystack[i + k] == needle[k])
+k++;
+else
+break;
+}
+if (needle[k])
+{
+i++;
+k = 0;
+}
+else
+return (haystack + i);
+}
+return (0);
+}
