45323d7cba63a196d7c01a4dbae0ba0f52f22efd
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-07 02:38:48 +0000

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - prints program
+* @argc: input
+* @argv: input
+* Return: output
+*/
+int main(int argc, char *argv[])
+	{
+	printf("%d\n", argc - 1);
+		if (argc == 0)
+		printf("%s\n", argv[argc]);
+	return (0);
+	}
+
+
+
+
+
