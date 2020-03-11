e6cb99f31bd692737718fd52c0783accfa18e940
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-11 10:02:43 +0000

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - makes a structure
+*@name: input
+*@owner: input
+*@age: input
+**/
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif
