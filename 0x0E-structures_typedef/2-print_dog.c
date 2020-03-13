65d3f94bda28bd2695ce305b637ff61f41744069
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-13 19:29:27 +0000

    commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - structure
+ * @d: input
+ */
+
+void print_dog(struct dog *d)
+
+{
+if (d != NULL)
+	{
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ float age;
 };
 
 void init_dog(struct dog *, char *, float, char *);
-
+void print_dog(struct dog *d);
 
 
 #endif
