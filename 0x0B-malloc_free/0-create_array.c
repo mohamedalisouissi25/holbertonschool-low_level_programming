e9755f36aa4549a72efc53ec36f952a66db80b3b
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-13 15:58:15 +0000

    commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* create_array - create array
+*
+* @size: input
+* @c: input
+* Return: output
+*/
+char *create_array(unsigned int size, char c)
+{
+char *a;
+unsigned int i;
+
+a = malloc(sizeof(char) * a);
+
+	if (s <= 0 || a == NULL)
+	return (NULL);
+	for (i = 0; i < a; i++)
+	{
+	a[i] = c;
+	}
+
+return (a);
+}
