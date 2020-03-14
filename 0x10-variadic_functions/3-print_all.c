95558e2c14ac20b83c0dafdcd4593066878a038a
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-14 19:11:34 +0000

    commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,92 @@
+#include "variadic_functions.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * print_all - prints anything
+ * Description: prints anything
+ * @n: input
+ * @separator: input
+ * Return: output
+ */
+void print_all(const char * const format, ...)
+
+va_list list;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+va_start
+
+
+va_end
