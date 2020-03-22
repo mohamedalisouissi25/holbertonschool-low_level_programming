0ce3c2c3333e918a31a757815feaefaee46945f8
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-22 19:29:21 +0000

    commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node? (- add a new node to the list)?
+ *
+ * @head: input
+ * @s: input
+ * Return: output
+ */
+
+list_t *add_node(list_t **head, const char *s)
+
+{
+unsigned int i;
+
+list_t *m = (list_t *)malloc(sizeof(list_t));
+
+	if (m == NULL)
+		return (NULL);
+	m->str = strdup(s);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	m->len = i;
+	m->next = *head;
+	*head = m;
+	return (m);
+}
