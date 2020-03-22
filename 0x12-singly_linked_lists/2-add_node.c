648d35aca0f3c3385a8a3b2b1a6a687f78ea50c7
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-03-22 19:31:09 +0000

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,13 +15,16 @@ unsigned int i;
 
 list_t *m = (list_t *)malloc(sizeof(list_t));
 
-	if (m == NULL)
-		return (NULL);
+if (m == NULL)
+	return (NULL);
+
 	m->str = strdup(s);
+
 	for (i = 0; s[i] != '\0'; i++)
 		;
 	m->len = i;
 	m->next = *head;
 	*head = m;
-	return (m);
+
+return (m);
 }
