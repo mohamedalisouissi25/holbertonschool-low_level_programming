031b0853344ace8a4c446794a134b57bd93761bc
mohamedalisouissi25 <dali-madridist@hotmail.com>
2020-05-02 22:43:40 +0000

    commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print the elements
+ * @h: input
+ * Return: output
+ */
+size_t print_dlistint(const dlistint_t *head)
+{
+	int i = 0;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		i++;
+	}
+	return (i);
+}
